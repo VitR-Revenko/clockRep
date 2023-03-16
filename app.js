f4a89e5bad5f259c557397dcdc720c22d7374b87
VitR-Revenko <re.vitalii002@gmail.com>
2023-03-16 22:49:38 +0200

    hw16

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ function updateClock() {
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
 
-    document.getElementById("hour-tens").src = "images/" + Math.floor(hours / 10) + ".png";
-    document.getElementById("hour-ones").src = "images/" + hours % 10 + ".png";
-    document.getElementById("minute-tens").src = "images/" + Math.floor(minutes / 10) + ".png";
-    document.getElementById("minute-ones").src = "images/" + minutes % 10 + ".png";
-    document.getElementById("second-tens").src = "images/" + Math.floor(seconds / 10) + ".png";
-    document.getElementById("second-ones").src = "images/" + seconds % 10 + ".png";
+    document.getElementById("hour-tens").src = `images/${Math.floor(hours / 10)}.png`;
+    document.getElementById("hour-ones").src = `images/${hours % 10}.png`;
+    document.getElementById("minute-tens").src = `images/${Math.floor(minutes / 10)}.png`;
+    document.getElementById("minute-ones").src = `images/${minutes % 10}.png`;
+    document.getElementById("second-tens").src = `images/${Math.floor(seconds / 10)}.png`;
+    document.getElementById("second-ones").src = `images/${seconds % 10}.png`;
 }
 
 updateClock();
